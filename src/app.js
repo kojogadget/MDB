14145679e53bd81fff210beff00f2ee91338a0e4
Kjartan Gundersen <kjartangundersen.kg@gmail.com>
2021-12-14 00:49:41 +0100

    Adding sass variables and footer

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./sass/main.scss";
+import Footer from "./components/Footer";
 
 const App = function () {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
