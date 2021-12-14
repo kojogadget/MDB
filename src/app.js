98955122536dca3e3e0c5654e37746d42b22ec14
Kjartan Gundersen <kjartangundersen.kg@gmail.com>
2021-12-14 23:52:34 +0100

    Creating pages folder

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import "./sass/main.scss";
+
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Home from "./components/Home";
-import MovieDetails from "./components/MovieDetails";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
 
 const App = function () {
   return (
