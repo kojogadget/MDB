e6bb80e29a3cd0a8f482692a00f969ba078c4b70
Kjartan Gundersen <kjartangundersen.kg@gmail.com>
2021-12-14 23:43:44 +0100

    edit styling nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,40 +3,37 @@ import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 import { NavData } from "./Nav.data";
-import { IconContext } from "react-icons";
 
 export default function Nav() {
-  const [sidebar, setSidebar] = useState(false);
+  const [navbar, setNavbar] = useState(false);
 
-  const showSideBar = () => setSidebar(!sidebar);
+  const showNavbar = () => setNavbar(!navbar);
 
   return (
     <>
-      <IconContext.Provider value={{ color: "#c0ccd" }}>
-        <div className="nav-btn">
-          <Link to="#" className="nav-btn__icon">
-            {sidebar ? (
-              <AiOutlineClose onClick={showSideBar} />
-            ) : (
-              <FaBars onClick={showSideBar} />
-            )}
-          </Link>
-        </div>
-        <nav className={sidebar ? "nav active" : "nav"}>
-          <ul className="nav__items" onClick={showSideBar}>
-            {NavData.map((item, i) => {
-              return (
-                <li key={i} className={item.className}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </IconContext.Provider>
+      <div className="nav-btn">
+        <Link to="#" className="nav-btn__icon">
+          {navbar ? (
+            <AiOutlineClose onClick={showNavbar} />
+          ) : (
+            <FaBars onClick={showNavbar} />
+          )}
+        </Link>
+      </div>
+      <nav className={navbar ? "nav active" : "nav"}>
+        <ul className="nav__items" onClick={showNavbar}>
+          {NavData.map((item, i) => {
+            return (
+              <li key={i} className={item.className}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </>
   );
 }
