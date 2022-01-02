8bac0a1f9be614405b065ff1fe115241b5a950d0
Kjartan Gundersen <kjartangundersen.kg@gmail.com>
2022-01-02 14:02:09 +0100

    Added pagination to related movies in MovieDetails

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,19 +1,51 @@
-import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
-import "../sass/pages/_movieDetails.scss";
-import "../sass/pages/_movies.scss";
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import '../sass/pages/_movieDetails.scss';
+import '../sass/pages/_movies.scss';
+
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+
+// const nextPage = () => {
+//   if (paginationPage === lastPage) setPaginationPage(1);
+//   setPaginationPage(paginationPage + 1);
+// };
+
+// const previousPage = () => {
+//   if (paginationPage === 1) setPaginationPage(lastPage);
+//   setPaginationPage(paginationPage - 1);
+// };
 
 export default function MovieDetails() {
   const { id } = useParams();
   const [movie, setMovie] = useState();
+  const [paginationPage, setPaginationPage] = useState(1);
+  const [resultPerPage, setResultPerPage] = useState(1);
+  const [lastPage, setLastPage] = useState(0);
+
+  let startPagination = (paginationPage - 1) * resultPerPage;
+  let endPagination = paginationPage * resultPerPage;
+
+  useEffect(() => {
+    const updateWidth = () => {
+      const width = window.innerWidth <= 1300 ? window.innerWidth : 1300;
+
+      console.log(Math.floor(width / 180));
+      setResultPerPage(Math.floor(width / 180));
+    };
+    updateWidth();
+
+    window.addEventListener('resize', updateWidth);
+
+    return () => window.removeEventListener('resize', updateWidth);
+  }, []);
 
   useEffect(async () => {
     const data = await fetch(
       `https://mcuapi.herokuapp.com/api/v1/movies/${id}`,
       {
-        method: "GET",
+        method: 'GET',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       }
     );
@@ -23,71 +55,129 @@ export default function MovieDetails() {
     setMovie(res);
   }, [id]);
 
+  useEffect(() => {
+    if (!movie) return;
+    console.log(movie.related_movies.length, resultPerPage);
+
+    const maxPages = Math.ceil(movie.related_movies.length / resultPerPage);
+
+    setLastPage(maxPages);
+    setPaginationPage(1);
+    startPagination = (paginationPage - 1) * resultPerPage;
+    endPagination = paginationPage * resultPerPage;
+  }, [movie, resultPerPage]);
+
   window.scrollTo(0, 0);
 
   return (
-    <div className="page">
-      <h1 className="page__header">{movie ? movie.title : "Loading..."}</h1>
+    <div className='page'>
+      <h1 className='page__header'>{movie ? movie.title : 'Loading...'}</h1>
       {movie ? (
-        <div className="movie">
-          <div className="movie__top-info">
-            <span className="movie__top-info--year">
+        <div className='movie'>
+          <div className='movie__top-info'>
+            <span className='movie__top-info--year'>
               {movie.release_date.slice(0, 4)}
             </span>
             /
-            <span className="movie__top-info--duration">
-              {Math.trunc(movie.duration / 60)}h{" "}
-              {movie.duration % 60 !== 0 ? (movie.duration % 60) + "m" : ""}
+            <span className='movie__top-info--duration'>
+              {Math.trunc(movie.duration / 60)}h{' '}
+              {movie.duration % 60 !== 0 ? (movie.duration % 60) + 'm' : ''}
             </span>
             /
-            <span className="movie__top-info--directed">
+            <span className='movie__top-info--directed'>
               {movie.directed_by}
             </span>
           </div>
 
-          <div className="movie__show">
-            <div className="movie__cover">
+          <div className='movie__show'>
+            <div className='movie__cover'>
               <img
                 src={movie.cover_url}
-                alt="Cover"
-                className="movie__cover--img"
+                alt='Cover'
+                className='movie__cover--img'
               />
             </div>
 
-            <div className="movie__trailer">
+            <div className='movie__trailer'>
               <iframe
                 src={movie.trailer_url}
-                className="movie__trailer--content"
+                className='movie__trailer--content'
               ></iframe>
             </div>
           </div>
 
-          <div className="movie__general">
-            <p className="article">{movie.overview}</p>
+          <div className='movie__general'>
+            <p className='article'>{movie.overview}</p>
           </div>
 
-          <div className="movie__related">
-            <h3 className="movie__related--heading">Related Movies</h3>
-            <ul className="movie__related--list">
-              {movie.related_movies.map((m, i) => {
-                return (
-                  <li key={i + m.imdb_id} className="movie__related--box">
-                    <Link
-                      to={"/movies/" + m.id}
-                      className="movie__related--link"
-                    >
-                      <img
-                        src={m.cover_url}
-                        alt="Cover"
-                        className="movie__related--img"
-                      />
-                      <div className="movie__related--info">
-                        <h3 className="movie__related--title">{m.title}</h3>
-                      </div>
-                    </Link>
-                  </li>
-                );
-              })}
+          <div className='movie__related'>
+            <button
+              className={
+                lastPage !== 1
+                  ? 'paginationBtn paginationBtn__back'
+                  : 'paginationBtn paginationBtn__back hidden'
+              }
+              onClick={() => {
+                if (paginationPage === 1) {
+                  setPaginationPage(lastPage);
+                  startPagination = (paginationPage - 1) * resultPerPage;
+                  endPagination = paginationPage * resultPerPage;
+                  return;
+                }
+
+                setPaginationPage(paginationPage - 1);
+                startPagination = (paginationPage - 1) * resultPerPage;
+                endPagination = paginationPage * resultPerPage;
+                console.log(paginationPage);
+              }}
+            >
+              <IoIosArrowBack />
+            </button>
+            <button
+              className={
+                lastPage !== 1
+                  ? 'paginationBtn paginationBtn__forward'
+                  : 'paginationBtn paginationBtn__forward hidden'
+              }
+              onClick={() => {
+                if (paginationPage === lastPage) {
+                  setPaginationPage(1);
+                  startPagination = (paginationPage - 1) * resultPerPage;
+                  endPagination = paginationPage * resultPerPage;
+                  return;
+                }
+
+                setPaginationPage(paginationPage + 1);
+                startPagination = (paginationPage - 1) * resultPerPage;
+                endPagination = paginationPage * resultPerPage;
+                console.log(paginationPage);
+              }}
+            >
+              <IoIosArrowForward />
+            </button>
+            <h3 className='movie__related--heading'>Related Movies</h3>
+            <ul className='movie__related--list'>
+              {movie.related_movies
+                .slice(startPagination, endPagination)
+                .map((m, i) => {
+                  return (
+                    <li key={i + m.imdb_id} className='movie__related--box'>
+                      <Link
+                        to={'/movies/' + m.id}
+                        className='movie__related--link'
+                      >
+                        <img
+                          src={m.cover_url}
+                          alt='Cover'
+                          className='movie__related--img'
+                        />
+                        <div className='movie__related--info'>
+                          <h3 className='movie__related--title'>{m.title}</h3>
+                        </div>
+                      </Link>
+                    </li>
+                  );
+                })}
             </ul>
           </div>
         </div>
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import "../sass/pages/_movies.scss";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import '../sass/pages/_movies.scss';
 
 export default function Movies() {
   const [movieList, setMovieList] = useState();
 
   useEffect(async () => {
     const data = await fetch(`https://mcuapi.herokuapp.com/api/v1/movies`, {
-      method: "GET",
+      method: 'GET',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     });
     const res = await data.json();
@@ -18,21 +18,21 @@ export default function Movies() {
   }, []);
 
   return (
-    <div className="page">
-      <h1 className="page__header">Movies</h1>
-      <ul className="movies">
+    <div className='page'>
+      <h1 className='page__header'>{movieList ? 'Movies' : 'Loading...'}</h1>
+      <ul className='movies'>
         {movieList
           ? movieList.map((movie, i) => {
               return (
-                <li key={i + movie.imdb_id} className="movies__box">
-                  <Link to={"/movies/" + movie.id} className="movies__link">
+                <li key={i + movie.imdb_id} className='movies__box'>
+                  <Link to={'/movies/' + movie.id} className='movies__link'>
                     <img
                       src={movie.cover_url}
-                      alt="Cover"
-                      className="movies__img"
+                      alt='Cover'
+                      className='movies__img'
                     />
-                    <div className="movies__info">
-                      <h3 className="movies__title">{movie.title}</h3>
+                    <div className='movies__info'>
+                      <h3 className='movies__title'>{movie.title}</h3>
                     </div>
                   </Link>
                 </li>
