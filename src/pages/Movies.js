a4175301c4b10c395eeefc98da99f1ef50c8c80d
Kjartan Gundersen <kjartangundersen.kg@gmail.com>
2022-01-05 14:36:12 +0100

    Added pagination to movies list

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 import '../sass/pages/_movies.scss';
 
 export default function Movies() {
   const [movieList, setMovieList] = useState();
+  const [paginationPage, setPaginationPage] = useState(0);
+  const [maxPages, setMaxPages] = useState(0);
+
+  const resultPerPage = 6;
+  let start = paginationPage * resultPerPage;
+  let end = start + resultPerPage;
 
   useEffect(async () => {
     const data = await fetch(`https://mcuapi.herokuapp.com/api/v1/movies`, {
@@ -15,14 +22,30 @@ export default function Movies() {
     const res = await data.json();
 
     setMovieList(res.data);
+    window.scrollTo(0, 0);
   }, []);
+
+  useEffect(() => {
+    if (!movieList) return;
+
+    const maxPages = Math.ceil(movieList.length / resultPerPage);
+
+    setMaxPages(maxPages);
+    setPaginationPage(0);
+    start = paginationPage * resultPerPage;
+    end = start + resultPerPage;
+  }, [movieList, resultPerPage]);
+
+  const changePage = ({ selected }) => {
+    setPaginationPage(selected);
+  };
 
   return (
     <div className='page'>
       <h1 className='page__header'>{movieList ? 'Movies' : 'Loading...'}</h1>
       <ul className='movies'>
         {movieList
-          ? movieList.map((movie, i) => {
+          ? movieList.slice(start, end).map((movie, i) => {
               return (
                 <li key={i + movie.imdb_id} className='movies__box'>
                   <Link to={'/movies/' + movie.id} className='movies__link'>
@@ -40,6 +63,17 @@ export default function Movies() {
             })
           : null}
       </ul>
+      <ReactPaginate
+        previousLabel={'<'}
+        nextLabel={'>'}
+        pageCount={maxPages}
+        onPageChange={changePage}
+        forcePage={paginationPage}
+        containerClassName={'movies__pagination'}
+        previousLinkClassName={'paginationBtn paginationBtn__back'}
+        nextLinkClassName={'paginationBtn paginationBtn__forward'}
+        activeLinkClassName={'activePagination'}
+      />
     </div>
   );
 }
